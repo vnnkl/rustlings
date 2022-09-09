61b0e9b75b6febe77a3ea6c1bd64e2e93177e9d9
Constantin Vennekel <constantin.vennekel@bitwala.com>
2022-09-09 16:33:03 +0200

    more progress

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -40,6 +38,18 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        //start with 0 for new one
+        let mut teamEntry1 = scores.entry(team_1_name.clone()).or_insert(Team{name:team_1_name.clone(),goals_scored:0,goals_conceded:0});
+        // update with result
+        teamEntry1.goals_scored += team_1_score;
+        teamEntry1.goals_conceded = team_2_score;
+
+        let mut teamEntry2 = scores.entry(team_2_name.clone()).or_insert(Team{name:team_2_name.clone(),goals_scored:0,goals_conceded:0});
+        teamEntry2.goals_scored += team_2_score;
+        teamEntry2.goals_conceded += team_1_score;
+
+
     }
     scores
 }
--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -1,8 +1,6 @@
 // options1.rs
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // you can modify anything EXCEPT for this function's signature
 fn print_number(maybe_number: Option<u16>) {
     println!("printing: {}", maybe_number.unwrap());
@@ -15,7 +13,14 @@ fn print_number(maybe_number: Option<u16>) {
 fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22
     // The Option output should gracefully handle cases where time_of_day > 24.
-    ???
+    
+    if time_of_day < 22 {
+        Some(5)
+    } else if time_of_day > 24 {
+        None
+    } else {
+        Some(0)
+    }
 }
 
 #[cfg(test)]
@@ -34,6 +39,6 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get at the value contained in the Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(icecreams.unwrap(), 5);
     }
 }
